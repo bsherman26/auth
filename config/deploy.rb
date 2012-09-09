d01e78713966efba7357bfa61d784e543d45bd9d
Brian Sherman <bsherman26@hotmail.com>
2012-09-09 02:48:37 -0500

    Initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+# GENERAL
+#default_run_options[:pty] = true                    # I had to add this so it would prompt for a password
+require "bundler/capistrano"                        # Add bundle install to deploy task
+load 'deploy/assets'                                # Link the folder specified in config.assets.prefix to shared/assets
+
+# APPLICATION
+set  :application, "auth"
+
+# REPOSITORY INFO
+set  :scm, :git
+set  :repository,  "git://github.com/bsherman26/auth"
+
+# SERVER INFO
+set  :deploy_to, "/apps/auth"            # Change from the default deploy directory ("/u/apps/#{application}")
+set  :user, "brian"                                 # This is not necessary, but if the user name was different it is needed
+set  :use_sudo, false                               # Prevent the use of sudo. This is important if the user specified below doesn't have sudo_privliges (I don't think sudo is needed for anything if privleges are set right and the user has database access)
+role :web, "basileis.com"                           # Your HTTP server, Apache/etc
+role :app, "basileis.com"                           # This may be the same as your `Web` server
+role :db,  "basileis.com", :primary => true         # This is where Rails migrations will run
+
+# ADDITIONAL TASKS
+namespace :deploy do                                # Default settings for Passenger restart etc. (# If you are using Passenger mod_rails uncomment this:)
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+
+ task :symlink_shared do                            # Added task to link shared database.yml into current release. This protects any passwords etc in the file.
+#   run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+ end
+end
+
+# ADDED TASK EXECUTION
+# after 'deploy:update_code', 'deploy:symlink_shared' # Run deploy:symlink_shared after running deploy:update_code
+
+
+# STUFF I DIDN'T USE
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+#role :db,  "your slave db-server here"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '54a14897f3d60f827bed83021d9eb03525d77a3cab208f01941ecd6808007ccfb45ed503d146ea4cf7e4eafeceddde86603b56b5ecb212aed14cce7c8311d7b8'
